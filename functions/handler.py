a2b3f731d08a148323d7d590e0fb81010be1a264
Cag <culucenk@mozilla.com>
2019-10-02 11:17:56 +1000

    Try to get tags consumed by the worker

--- functions/handler.py
+++ functions/handler.py
@@ -18,13 +18,11 @@ def lambda_handler(event, context):
     api_event = event
     returnDict = dict()
     returnDict['details'] = {}
-    # Tags need to be list
-    returnDict['tags'] = []
 
     # See if submitted POST body is a valid JSON and has
     #  a "payload" since this is common in webhook data
     try:
-        json.loads(api_event['body'])
+        message = json.loads(api_event['body'])
     except Exception as e:
         logger.error("Cannot parse API event: {}, context: {}, exception: ".format(
             str(api_event['body']),
@@ -34,26 +32,40 @@ def lambda_handler(event, context):
                 'statusCode': 400,
                 'body': json.dumps('Bad Request')
         }
-    if 'payload' not in json.loads(api_event['body']).keys():
+    if 'payload' not in message.keys():
         logger.error("Expecting a payload for the event, but not found.")
         return {
                 'statusCode': 400,
                 'body': json.dumps('Bad Request')
         }
+    returnDict = {
+        'hostname': '{}_host'.format(service),
+        'processname': service,
+        'processid': 'none',
+        'severity': 'INFO',
+        'summary': '{}_event'.format(service),
+        'category': service,
+        'source': 'api_aws_lambda',
+        'eventsource': service + '_api',
+        'tags': [
+            service
+        ],
+        'details': message
+    }
 
-    returnDict['summary'] = '{}_event'.format(service)
-    returnDict['source'] = 'api_aws_lambda'
-    returnDict['category'] = service
-    returnDict['eventsource'] = service + '_api'
-    returnDict['hostname'] = '{}_host'.format(service)
-    returnDict['tags'].append(service)
-    returnDict['processname'] = service
-    returnDict['processid'] = 'none'
-    returnDict['severity'] = 'INFO'
-    returnDict['details'] = json.loads(api_event['body'])
+    # returnDict['summary'] = '{}_event'.format(service)
+    # returnDict['source'] = 'api_aws_lambda'
+    # returnDict['category'] = service
+    # returnDict['eventsource'] = service + '_api'
+    # returnDict['hostname'] = '{}_host'.format(service)
+    # returnDict['tags'] = [ service ]
+    # returnDict['processname'] = service
+    # returnDict['processid'] = 'none'
+    # returnDict['severity'] = 'INFO'
+    # returnDict['details'] = message
 
     queueURL = os.getenv('SQS_URL')
-    sqs.send_message(QueueUrl=queueURL, MessageBody=returnDict)
+    sqs.send_message(QueueUrl=queueURL, MessageBody=json.dumps(returnDict))
     logger.info("Event added to the resource queue.")
 
     return {
