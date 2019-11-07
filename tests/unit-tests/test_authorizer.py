8163b0e7fd14016852e0869f28018b8ba9fdcccb
Cag <culucenk@mozilla.com>
2019-11-07 11:09:04 +1100

    Create skeleton test files

---
+++ tests/unit-tests/test_authorizer.py
@@ -0,0 +1,29 @@
+import json
+import pytest
+import boto3
+from moto import mock_ssm, mock_cloudformation
+from functions import authorizer
+
+
+@pytest.fixture()
+def test_generate_policy():
+
+
+
+
+    return True
+
+
+def test_validate_token():
+
+
+
+    return True
+
+@mock_ssm
+def test_get_auth_token():
+
+
+
+
+    return True
---
+++ tests/unit-tests/test_handler.py
@@ -0,0 +1,13 @@
+import json
+import pytest
+import boto3
+from moto import mock_sqs
+from functions import handler
+
+
+def test_lambda_handler():
+
+
+
+
+    return True
